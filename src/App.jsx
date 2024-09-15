2daf489706977eacbb4485f8b2f46d0460fc8b62
roshani480 <rosh.angel692@gmail.com>
2024-09-15 14:47:02 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1> this is new job portal app</h1>
+    <h2> this is old job portal app</h2>
+     
+    </>
+  )
+}
+
+export default App
