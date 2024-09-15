0bf330fdb516134025d18185e1428f6a8b3b736b
roshani480 <rosh.angel692@gmail.com>
2024-09-15 15:38:30 +0000

    h4 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-    <h1> this is new job portal app</h1>
-    <h2> this is old job portal app</h2>
-    <h3> this is old job portal app</h3>
-     
+      <h1> this is new job portal app</h1>
+      <h2> this is old job portal app</h2>
+      <h3> this is old job portal app</h3>
+      <h4> this is old job portal app</h4>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
